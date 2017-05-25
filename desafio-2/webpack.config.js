72135f7854ec68f00083d201a3cb916e6ed428fe
ricardo.jsx <rsbramb@br.ibm.com>
2017-05-25 20:52:21 -0300

    Initial Commit

---
+++ desafio-2/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractSass = new ExtractTextPlugin({filename: "./style.css"});
+
+const PATHS = {
+    app: path.join(__dirname, 'src'),
+    build: path.join(__dirname, 'build')
+};
+
+module.exports = {
+    devtool: "source-map",
+    entry: {
+        app: PATHS.app,
+    },
+    output: {
+        path: PATHS.build,
+        filename: 'bundle.js',
+    },
+    devServer: {
+        inline: true,
+        contentBase: PATHS.build,
+        port: 3333
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: extractSass.extract({
+                    use: [{loader: "css-loader"}, {loader: "sass-loader"}]
+                })
+            }
+        ]
+    },
+    plugins: [
+        extractSass
+    ]
+};
