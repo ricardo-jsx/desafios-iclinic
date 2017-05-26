6578fd3074b36a70c51373408ea8cdc6d66ababe
ricardo.jsx <rsbramb@br.ibm.com>
2017-05-25 23:50:50 -0300

    Uses redux store and provider

--- desafio-2/src/index.js
+++ desafio-2/src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 
-import App from './App';
+import store from './data/root/store';
+import App from './container/App';
 
-ReactDOM.render(<App name="John" />, document.getElementById('app'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("app")
+);
 
-if(module.hot) module.hot.accept();
+if (module.hot) {
+    module.hot.accept('./container/App', () => {
+        const NextApp = require("./container/App").default;
+
+        ReactDOM.render(
+            <Provider store={store}>
+                <NextApp/>
+            </Provider>, document.getElementById('app')
+        );
+    });
+}
