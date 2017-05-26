13faf7fb414d7046cac3cfa76e1b3623be5f6756
ricardo.jsx <rsbramb@br.ibm.com>
2017-05-26 02:23:10 -0300

    Combines project's reducers

--- desafio-2/src/data/root/reducer.js
+++ desafio-2/src/data/root/reducer.js
@@ -1,5 +1,7 @@
 import {combineReducers} from 'redux';
 
 import order from '../order/reducer';
+import todo from '../todo/reducer';
+import visibility from '../visibility/reducer';
 
-export default combineReducers({order});
+export default combineReducers({order, todo, visibility});
