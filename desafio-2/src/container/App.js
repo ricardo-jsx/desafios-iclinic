8a6bc76d2d385555a57e5a42d1f7f32d2e7dd33a
ricardo.jsx <rsbramb@br.ibm.com>
2017-05-26 02:22:56 -0300

    Uses a header inside app

--- desafio-2/src/container/App.js
+++ desafio-2/src/container/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import Paper from 'material-ui/Paper'
 import {connect} from 'react-redux';
 
-import RaisedButton from 'material-ui/RaisedButton';
+import Header from './Header';
 
 import './style/App.scss';
 
-const App = ({name}) => {
+const style = {
+    width: 500,
+    margin: 20,
+    flex: 1,
+};
+
+const App = () => {
     return (
         <div className="fill-screen">
-            <div>
-                <RaisedButton label="Default"/>
-            </div>
+            <Paper style={style} zDepth={3}>
+                <Header/>
+                <div style={{flex: 8, backgroundColor: 'pink'}}></div>
+                <div style={{flex: 1, backgroundColor: 'purple'}}></div>
+            </Paper>
         </div>
     )
 };
