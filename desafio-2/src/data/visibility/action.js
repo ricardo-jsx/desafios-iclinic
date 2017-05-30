ac58d0f424ca94b0582ebda7112338579d29e871
ricardo.jsx <rsbramb@br.ibm.com>
2017-05-30 02:29:35 -0300

    Converts json to function

--- desafio-2/src/data/visibility/action.js
+++ desafio-2/src/data/visibility/action.js
@@ -1,10 +1,10 @@
 export const SHOW_COMPLETED = "SHOW_COMPLETED";
 export const SHOW_ALL = "SHOW_ALL";
 
-export const showCompleted = ({
+export const showCompleted = () => ({
     type: SHOW_COMPLETED
 });
 
-export const showAll = ({
+export const showAll = () => ({
     type: SHOW_ALL
 });
