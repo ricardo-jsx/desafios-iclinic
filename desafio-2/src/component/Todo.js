6760f77c2ddc8d9e7cc670bcace4e6272e5d5a9b
ricardo.jsx <rsbramb@br.ibm.com>
2017-05-30 02:29:08 -0300

    Fix css of todo

--- desafio-2/src/component/Todo.js
+++ desafio-2/src/component/Todo.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Checkbox from 'material-ui/Checkbox';
-import Delete from './DeleteTodo';
+import Delete from 'material-ui/svg-icons/action/delete';
+
+import TodoAction from './TodoAction';
 import Divider from 'material-ui/Divider';
 
-const Todo = ({text, completed, onCheck}) => {
+import './style/Todo.scss';
+
+const Todo = ({text, completed, onCheck, onDelete}) => {
 
     return (
         <div>
-            <div style={{display: 'flex', flexDirection: 'row', marginLeft: '20px', flex: 1}}>
-                <Delete />
+            <div className="todo">
+                <TodoAction onClick={onDelete}>
+                    <Delete />
+                </TodoAction>
                 <Checkbox
                     label={text}
                     checked={completed}
-                    style={{heigth: '48px', padding: '12px'}}
+                    style={{heigth: '48px', padding: '12px', display: "flex"}}
                     onCheck={onCheck}
                 />
+
+                <div style={{display: 'flex'}}> {' '}</div>
             </div>
             <Divider />
         </div>
@@ -25,6 +33,7 @@ const Todo = ({text, completed, onCheck}) => {
 Todo.propTypes = {
     text: PropTypes.string.isRequired,
     onCheck: PropTypes.func.isRequired,
+    onDelete: PropTypes.func.isRequired,
     completed: PropTypes.bool,
 };
 
